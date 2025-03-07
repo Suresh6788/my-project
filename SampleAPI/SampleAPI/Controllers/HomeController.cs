befe0ad91b9740e04dd93654189f5a8af3667e84
Suresh6788 <suresh.lammatha@gmail.com>
2025-03-07 14:10:58 +0530

    start

---
+++ SampleAPI/SampleAPI/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using EmployeeAPI.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SampleAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        private readonly ApplicationDbContext context;
+
+        public HomeController(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+
+
+    }
+}
